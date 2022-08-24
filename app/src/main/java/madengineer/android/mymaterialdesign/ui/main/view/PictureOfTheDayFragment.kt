9c535e6dc91edb45f55ae9aa596c231ea80631bc
Den <etlendo@mail.ru>
2022-08-24 21:19:18 +0500

    Урок 1 | Создал и применил стили для компонентов

--- app/src/main/java/madengineer/android/mymaterialdesign/ui/main/view/PictureOfTheDayFragment.kt
+++ app/src/main/java/madengineer/android/mymaterialdesign/ui/main/view/PictureOfTheDayFragment.kt
@@ -67,7 +67,12 @@ class PictureOfTheDayFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.app_bar_fav -> toast("Favourite")
-            R.id.app_bar_settings -> toast("Settings")
+            R.id.app_bar_settings -> activity
+                ?.supportFragmentManager
+                ?.beginTransaction()
+                ?.replace(R.id.container, SettingsFragment())
+                ?.addToBackStack(null)
+                ?.commit()
             android.R.id.home -> {
                 activity?.let {
                     BottomNavigationDrawerFragment().show(it.supportFragmentManager, "tag")
