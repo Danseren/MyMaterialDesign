9a77da70f6ed0a07ce734f43cbc3df0e10530154
Danseren <104249031+Danseren@users.noreply.github.com>
2022-10-22 16:36:36 +0300

    Урок 5 | Добавил движение элементов fragment_settings

--- app/src/main/java/madengineer/android/mymaterialdesign/ui/main/view/SettingsFragment.kt
+++ app/src/main/java/madengineer/android/mymaterialdesign/ui/main/view/SettingsFragment.kt
@@ -5,13 +5,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnticipateOvershootInterpolator
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.fragment.app.Fragment
+import androidx.transition.ChangeBounds
+import androidx.transition.TransitionManager
 import coil.load
 import com.google.android.material.snackbar.Snackbar
 import madengineer.android.mymaterialdesign.MainActivity
 import madengineer.android.mymaterialdesign.R
 import madengineer.android.mymaterialdesign.databinding.FragmentSettingsBinding
-import madengineer.android.mymaterialdesign.ui.main.model.PODServerResponseData
 import madengineer.android.mymaterialdesign.ui.main.viewmodel.AppState
 import madengineer.android.mymaterialdesign.ui.main.viewmodel.MainViewModel
 
@@ -20,6 +23,7 @@ class SettingsFragment : Fragment() {
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
     lateinit var mainViewModel: MainViewModel
+    var isFlag = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,8 +38,35 @@ class SettingsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         mainViewModel.getData().observe(viewLifecycleOwner, {renderData(it)})
         mainViewModel.getPOD(0)
+
+        val constraintSet = ConstraintSet()
+        constraintSet.clone(requireContext(), R.layout.fragment_settings)
+
         binding.btnFirst.setOnClickListener {
             binding.textViewDescription.textSize = 8F
+            isFlag = !isFlag
+            val chageBounds = ChangeBounds()
+            chageBounds.duration = 2000L
+            chageBounds.interpolator = AnticipateOvershootInterpolator(5.0f)
+            TransitionManager.beginDelayedTransition(binding.constraintContainer, chageBounds)
+            if (isFlag) {
+                constraintSet.apply {
+                    connect(R.id.textViewConstraint, ConstraintSet.END, R.id.constraint_container, ConstraintSet.END)
+                    connect(R.id.btnFirst, ConstraintSet.TOP, R.id.textViewConstraint, ConstraintSet.BOTTOM)
+                    connect(R.id.btnFirst, ConstraintSet.END, R.id.constraint_container, ConstraintSet.END)
+                    connect(R.id.btnSecond, ConstraintSet.BOTTOM, R.id.constraint_container, ConstraintSet.BOTTOM)
+                    connect(R.id.btnThird, ConstraintSet.TOP, R.id.btnSecond, ConstraintSet.BOTTOM)
+                }
+            } else {
+                constraintSet.apply {
+                    clear(R.id.textViewConstraint, ConstraintSet.END)
+                    clear(R.id.btnFirst, ConstraintSet.END)
+                    connect(R.id.btnFirst, ConstraintSet.TOP, R.id.guideline, ConstraintSet.BOTTOM)
+                    clear(R.id.btnSecond, ConstraintSet.BOTTOM)
+                    clear(R.id.btnThird, ConstraintSet.TOP)
+                }
+            }
+            constraintSet.applyTo(binding.constraintContainer)
         }
         binding.btnSecond.setOnClickListener {
             binding.textViewDescription.textSize = 12F
